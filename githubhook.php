f478205c4c32c032bc45b369993be6e0d6d3761e
Thomas Buck <taucher.bodensee@googlemail.com>
2012-03-08 18:53:52 +0100

    Added log

--- githubhook.php
+++ githubhook.php
@@ -6,12 +6,45 @@
 	 *
 	 * Just enter some addresses after this comment.
 	 * They'll get a mail for every commit!
+	 * Also shows all commits if you enter your mysql credentials
 	 */
+	/*
+	 * SQL to create table
+CREATE TABLE commits (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL,
+	mail TEXT,
+	message TEXT,
+	url TEXT NOT NULL,
+	repo TEXT NOT NULL )
+	 */
+	$server = "mysql5.service";
+	$user = "HTO01FLBDAFU_app";
+	$pass = "PASSWORD GOES HERE";
+	$database = "HTO01FLBDAFU_app";
 
 	$to = "xythobuz@me.com, hutattedonmyarm@me.com, baeder.felix@gmail.com";
 
 	if (!array_key_exists('payload', $_POST)) {
-		echo "<p>No Payload</p></body></html>";
+		if (isset($server) && isset($user) && isset($pass) && isset($database)) {
+			$sql = 'SELECT * FROM commits';
+			$result = mysql_query($sql);
+			if ($result) {
+				echo "<h1>Commits</h1>";
+				while ($row = mysql_fetch_array($result)) {
+					echo "<p>From: ".$row['name']." (".$row['mail'].")</p>";
+					echo "<p>".$row['message']."</p>";
+					echo "<p><a href=\"".$row['mail']."\">Go to commit in ".$row['repo']."</a></p>";
+					echo "<hr>";
+				}
+				echo "</body></html>";
+			} else {
+				echo "<p>Database error...</p></body></html>";
+				exit;
+			}
+		} else {
+			echo "<p>No payload!</p></body></html>";
+		}
 		exit;
 	}
 	
@@ -37,8 +70,17 @@
 		$msg = $commit['message'];
 		$name = $commit['author']['name'];
 		$url = $commit['url'];
+		if (isset($server) && isset($user) && isset($pass) && isset($database)) {
+			$sql = 'INSERT INTO commits
+				name = "'.$name.'",
+				mail = "'.$from.'",
+				message = "'.$msg.'",
+				url = "'.$url.'",
+				repo = "'.$repo.'"';
+			$result = mysql_query($sql);
+		}
 		sendEmail($to, $from, $msg, $name, $url, $repo);
-		echo "<p>Mail sent!</p>\n";
+		echo "<p>Done!</p>\n";
 	}
 
 	function sendEmail($to, $from, $msg, $name, $url, $repo) {
