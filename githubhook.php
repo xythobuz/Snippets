174334e532aa01410c11916697fe4cf570dbac50
Thomas Buck <taucher.bodensee@googlemail.com>
2012-06-01 16:24:32 +0200

    Major githubhook rewrite.

--- githubhook.php
+++ githubhook.php
@@ -2,15 +2,18 @@
 <?php
 	/*
 	 * GitHub Post Recieve Hook in PHP
-	 * Copy & Pasted from the interwebz by xythobuz
+	 * By xythobuz
 	 *
-	 * Just enter some addresses after this comment.
-	 * They'll get a mail for every commit!
-	 * Also shows all commits if you enter your mysql credentials.
-	 * Delete vars if you don't want this!
+	 * Shows Commits of Github projects, sends emails to desired people.
+	 * Installation:
+	 * 1) Run the following 2 SQL Statements to create the needed tables
+	 * 2) Enter your SQL Server credentials after the comments in this file.
+	 * 3) Load this file onto your webserver
+	 * 4) Point the Github Post Recieve Hook to this script.
 	 */
 	/*
-	 * SQL to create table
+	 * SQL to create table:
+
 CREATE TABLE commits (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	name TEXT NOT NULL,
@@ -18,104 +21,88 @@ CREATE TABLE commits (
 	message TEXT,
 	url TEXT NOT NULL,
 	repo TEXT NOT NULL )
+
+CREATE TABLE mail (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	mail TEXT NOT NULL,
+	repo TEXT NOT NULL )
+
 	 */
-	$server = "HOSTNAME";
+	$server = "SERVERNAME";
 	$user = "USERNAME";
 	$pass = "PASSWORD";
 	$database = "DATABASE";
 
-	$to = "test@test.com, test@example.org";
-
-	if (isset($server) && isset($user) && isset($pass) && isset($database)) {
-		$db = mysql_connect($server, $user, $pass);
-		mysql_select_db($database);
-		if (mysql_errno()) {
-			echo ('Database error!');
-			unset($server);
-			unset($user);
-			unset($pass);
-			unset($database);
-		}
+	$db = mysql_connect($server, $user, $pass);
+	mysql_select_db($database);
+	if (mysql_errno()) {
+		echo ('Database error!');
+		exit;
 	}
 
 	if (!array_key_exists('payload', $_POST)) {
-		if (isset($server) && isset($user) && isset($pass) && isset($database)) {
-			if (!isset($_GET['r'])) {
-				?><fieldset><label>Repository: <form method="get"><select name="r"><option value="">All Repos</option><?
-				$sql = 'SELECT repo FROM commits GROUP BY repo';
+		// Were not called as recieve hook --> Show stuff
+
+?>
+<p><a href="http://www.xythobuz.org">GutHub Post Recieve Hook in PHP by xythobuz</a></p>
+<p>Repository:
+<form method="get">
+<? showSelect(); ?>
+<input type="submit" value="Show commits">
+</form></p>
+<p>Mail Notifications:
+<form method="get">
+<? showSelect(0); ?>
+<br><input type="text" name="mail">
+<input type="submit" value="Subscribe Repo" name="subscribe">
+</form></p>
+<?
+		if (isset($_GET['subscribe'])) {
+			// Subscripe person
+			if (($_GET['mail'] != "") && ($_GET['r'] != "")) {
+				$sql = 'INSERT INTO mail(mail, repo)
+				VALUES
+					( "'.mysql_real_escape_string($_GET['mail']).'",
+					"'.mysql_real_escape_string($_GET['r']).'" )';
 				$result = mysql_query($sql);
 				if (!$result) {
-					echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
-					exit;
-				}
-				while ($row = mysql_fetch_array($result)) {
-					?><option value="<? echo $row['repo']; ?>"><? echo $row['repo']; ?></option><?
-				}
-				?></select><input type="submit" value="Show commits" /></label></form></fieldset><?
-				
-				$sql = 'SELECT *
-					FROM commits
-					ORDER BY id DESC';
-				$result = mysql_query($sql);
-				if ($result) {
-					echo "<h1>Commits</h1><hr>";
-					while ($row = mysql_fetch_array($result)) {
-						echo "<h2>".$row['repo']."</h2>";
-						echo "<p>From: ".$row['name']." (".$row['mail'].")</p>";
-						echo "<p>".$row['message']."</p>";
-						echo "<p><a href=\"".$row['url']."\">Go to commit in ".$row['repo']."</a></p>";
-						echo "<hr>";
-					}
-					echo "</body></html>";
-					exit;
+					echo "<p>Database error!</p></body></html>";
 				} else {
-					echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
-					exit;
+					echo "<p>You are now on the list!</p></body></html>";
 				}
 			} else {
-				?><fieldset><label>Repository: <form method="get"><select name="r"><option value="">All Repos</option><?
-				$sql = 'SELECT repo FROM commits GROUP BY repo';
-				$result = mysql_query($sql);
-				if (!$result) {
-					echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
-					exit;
-				}
-				while ($row = mysql_fetch_array($result)) {
-					?><option value="<? echo $row['repo']; ?>"><? echo $row['repo']; ?></option><?
-				}
-				?></select><input type="submit" value="Show commits" /></label></form></fieldset><?
-
-				if ($_GET['r'] == "") {
-					$sql = 'SELECT *
-					FROM commits
-					ORDER BY id DESC';
-				} else {
-					$sql = 'SELECT *
-					FROM commits
-					WHERE repo = "'.mysql_real_escape_string($_GET['r']).'"
-					ORDER BY id DESC';
-				}
-				$result = mysql_query($sql);
-				if ($result) {
-					echo "<h1>Commits</h1><hr>";
-					while ($row = mysql_fetch_array($result)) {
-						echo "<h2>".$row['repo']."</h2>";
-						echo "<p>From: ".$row['name']." (".$row['mail'].")</p>";
-						echo "<p>".$row['message']."</p>";
-						echo "<p><a href=\"".$row['url']."\">Go to commit in ".$row['repo']."</a></p>";
-						echo "<hr>";
-					}
-					echo "</body></html>";
-					exit;
-				} else {
-					echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
-					exit;
-				}
+				echo "<p>Invalid data!</p></body></html>";
 			}
+			exit;
 		} else {
-			echo "<p>No payload!</p></body></html>";
+			// Show commits
+			if ((!isset($_GET['r'])) || ($_GET['r'] == "")) {
+				$sql = 'SELECT *
+				FROM commits
+				ORDER BY id DESC';
+			} else {
+				$sql = 'SELECT *
+				FROM commits
+				WHERE repo = "'.mysql_real_escape_string($_GET['r']).'"
+				ORDER BY id DESC';
+			}
+			$result = mysql_query($sql);
+			if ($result) {
+				echo "<h1>Commits</h1><hr>";
+				while ($row = mysql_fetch_array($result)) {
+					echo "<h2>".stripslashes($row['repo'])."</h2>";
+					echo "<p>From: ".stripslashes($row['name'])." (".stripslashes($row['mail']).")</p>";
+					echo "<p>".stripslashes($row['message'])."</p>";
+					echo "<p><a href=\"".stripslashes($row['url'])."\">Go to commit in ".stripslashes($row['repo'])."</a></p>";
+					echo "<hr>";
+				}
+				echo "</body></html>";
+				exit;
+			} else {
+				echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
+				exit;
+			}
 		}
-		exit;
 	}
 	
 	extract(json_decode(stripslashes($_POST['payload']), true));
@@ -140,18 +127,29 @@ CREATE TABLE commits (
 		$msg = $commit['message'];
 		$name = $commit['author']['name'];
 		$url = $commit['url'];
-		if (isset($server) && isset($user) && isset($pass) && isset($database)) {
-			$sql = 'INSERT INTO
-				commits(name, mail, message, url, repo)
-			VALUES
-				("'.mysql_real_escape_string($name).'",
-				"'.mysql_real_escape_string($from).'",
-				"'.mysql_real_escape_string($msg).'",
-				"'.mysql_real_escape_string($url).'",
-				"'.mysql_real_escape_string($repo).'")';
-			$result = mysql_query($sql);
+		$sql = 'INSERT INTO
+			commits(name, mail, message, url, repo)
+		VALUES
+			("'.mysql_real_escape_string($name).'",
+			"'.mysql_real_escape_string($from).'",
+			"'.mysql_real_escape_string($msg).'",
+			"'.mysql_real_escape_string($url).'",
+			"'.mysql_real_escape_string($repo).'")';
+		$result = mysql_query($sql);
+		if (!$result) {
+			echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
+			exit;
 		}
-		sendEmail($to, $from, $msg, $name, $url, $repo);
+
+		$sql = 'SELECT mail, repo FROM mail WHERE repo = "'.mysql_real_escape_string($repo).'"';
+		$to = "";
+		while ($row = mysql_fetch_array($result)) {
+			$to .= stripslashes($row['mail']);
+		}
+
+		if ($to != "") {
+			sendEmail($to, $from, $msg, $name, $url, $repo);
+		}
 		echo "<p>Done!</p>\n";
 	}
 
@@ -172,6 +170,24 @@ CREATE TABLE commits (
 		$header .= "Reply-To: ".$antwortan."\r\n";
 		$header .= "X-Mailer: PHP ".phpversion();
 		mail( $to, $betreff, $mailtext, $header);
+	}
+
+	function showSelect($i = 1) {
+?><select name="r">
+<? if ($i == 1) { ?>
+<option value="">All Repos</option>
+<? }
+		$sql = 'SELECT repo FROM commits GROUP BY repo';
+		$result = mysql_query($sql);
+		if (!$result) {
+			echo "<p>Database error!</p><p>".mysql_error()."</p></body></html>";
+			exit;
+		}
+		while ($row = mysql_fetch_array($result)) {
+?><option value="<? echo stripslashes($row['repo']); ?>"<? if ($row['repo'] == $_GET['r']) { echo " selected"; } ?>><? echo stripslashes($row['repo']); ?></option><?
+		}
+?></select>
+<?
 	}
 ?>
 </body></html>
