0dac472996a174d78a489fe1c0d536857ffe5fbe
Thomas Buck <taucher.bodensee@googlemail.com>
2013-02-11 10:46:14 +0100

    ipnotify fix

--- ipnotify.sh
+++ ipnotify.sh
@@ -14,8 +14,7 @@ if [ "$ip" != "$lastip" ]; then
     if [ "$ip" != "" ]; then
         echo "New public IP. Sending notification!"
         curl -s "https://www.ultimatenotifier.com/items/User/send/${user}/message=${message}%20${ip}/password=${pass}"
+        cp /dev/null $ipfile
+        echo $ip > $ipfile
     fi
 fi
-
-cp /dev/null $ipfile
-echo $ip > $ipfile
