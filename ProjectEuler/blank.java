c5856856f6bb299bb1f71bc6b4c636a22da74d56
Thomas Buck <xythobuz@xythobuz.de>
2013-10-31 23:59:31 +0100

    Fixed indentation

--- ProjectEuler/blank.java
+++ ProjectEuler/blank.java
@@ -1,36 +1,36 @@
 public class ID1 {
-	public static void main (String[] args) {
-		java.util.Date start = new java.util.Date();
-		// -------------------------------------------
-		// ----------------- Solution ----------------
-		// -------------------------------------------
+    public static void main (String[] args) {
+        java.util.Date start = new java.util.Date();
+        // -------------------------------------------
+        // ----------------- Solution ----------------
+        // -------------------------------------------
 
-		// -------------------------------------------
-		java.util.Date stop = new java.util.Date();
-		printTime(start, stop);
-	}
-	
-	static void printTime(java.util.Date start, java.util.Date stop) {
-		long diff = stop.getTime() - start.getTime();
-		int diffsec = 0;
-		int diffmin = 0;
-		while (diff > 1000) {
-			diff -= 1000;
-			diffsec++;
-		}
-		while (diffsec > 60) {
-			diffsec -= 60;
-			diffmin++;
-		}
-		System.out.println();
-		if (diffmin > 0) {
-			System.out.println("Minutes: " + diffmin);
-		}
-		if (diffsec > 0) {
-			System.out.println("Seconds: " + diffsec);
-		}
-		if (diff > 0) {
-			System.out.println("Milliseconds: " + diff);
-		}
-	}
+        // -------------------------------------------
+        java.util.Date stop = new java.util.Date();
+        printTime(start, stop);
+    }
+
+    static void printTime(java.util.Date start, java.util.Date stop) {
+        long diff = stop.getTime() - start.getTime();
+        int diffsec = 0;
+        int diffmin = 0;
+        while (diff > 1000) {
+            diff -= 1000;
+            diffsec++;
+        }
+        while (diffsec > 60) {
+            diffsec -= 60;
+            diffmin++;
+        }
+        System.out.println();
+        if (diffmin > 0) {
+            System.out.println("Minutes: " + diffmin);
+        }
+        if (diffsec > 0) {
+            System.out.println("Seconds: " + diffsec);
+        }
+        if (diff > 0) {
+            System.out.println("Milliseconds: " + diff);
+        }
+    }
 }
